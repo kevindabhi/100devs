3e6a94c3d997e27d831d8bb304afd3c744181c68
Kevin Dabhi <36810148+kevindabhi@users.noreply.github.com>
2021-08-06 09:47:37 +0200

    Add files via upload

---
+++ theJsWay/chapter07.js
@@ -0,0 +1,64 @@
+// const musketeers = ["Athos", "Porthos", "Aramis"];
+// for (let i = 0; i < musketeers.length; i++) {
+// console.log(musketeers[i]);
+
+// }
+
+// musketeers.push("D’Artagnan");
+
+// musketeers.forEach(musketeers => {
+//     console.log(musketeers);
+// });
+
+
+// musketeers.splice(2,1);
+
+
+// for(const musketeer of musketeers)
+// {
+//     console.log(musketeer);
+// }
+
+
+//Sum of values.
+
+// const values = [3,11,7,2,9,10];
+
+// let total=0;
+// for(const i in values) { total = total + values[i]; 
+
+// }
+
+// console.log(`${total}`)
+
+// const values = [3,11,7,2,9,10];
+// let total = 0;
+// for (let i = 0; i < values.length; i++) {
+//    total = total + values[i];
+
+// }
+// console.log(`${total}`);
+
+
+//finding the biggest number
+
+// const values = [3, 11, 7, 2, 9, 10];
+// let biggest = 0 ;
+// for(const i in values) {
+//     if (biggest<=values[i]){
+//             biggest=values[i];
+
+//     }
+
+// }
+//     console.log(`${biggest}`);
+
+//Enter wirds until ''stop''
+
+    // var words = [], answer, i;
+    
+    // while ((answer = prompt('Enter a word')) != "stop") words.push(answer);
+    
+    // for (i = 0; i < words.length; i++) console.log(words[i] );
+    
+ 
---
+++ theJsWay/chapter08.js
@@ -0,0 +1,76 @@
+// console.log("Hey");
+
+// const str = "This is a string.";
+// const len = str.length;
+// console.log(len);
+
+// const lowerCase = str.toLowerCase();
+// console.log(lowerCase);
+
+// const upperCase = lowerCase.toUpperCase();
+// console.log(upperCase)
+
+
+// const country = "Deutschland";
+
+// for ( i=0; i< country.length; i++)
+// {
+//     console.log[i];
+// }
+
+// word info
+
+// let string = prompt("Enter a string");
+
+// const lowerCase= string.toLowerCase();
+// const upperCase= string.toUpperCase();
+
+// console.log(lowerCase);
+// console.log(upperCase);
+// console.log(string.length);
+// const vowel= []
+
+// for (const i of string) {
+    
+//     if (i === "a") {
+//         vowel.push(i);
+//     } else if (i === "e"){
+//         vowel.push(i);
+//     }
+    
+//     else if (i === "i"){
+//         vowel.push(i);
+//     }
+//     else if (i === "o"){
+//         vowel.push(i);
+//     }
+//     else if (i === "u"){
+//         vowel.push(i);
+//     }
+
+// }
+
+// console.log(`Vowel length is ${vowel.length}`);
+
+
+
+
+
+// var splitString = string.split("");
+// var reverseArray = splitString.reverse();
+// var joinArray = reverseArray.join("");
+// console.log(joinArray);
+
+
+
+
+// if (string===joinArray)
+
+// {
+//     console.log("palindrome");
+// }
+
+// else{
+
+//     console.log("not palindrome");
+// }
