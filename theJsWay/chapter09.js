bfe70a966fc5bd7c3d5a81fe129b6a012863e04a
Kevin Dabhi <36810148+kevindabhi@users.noreply.github.com>
2021-08-08 09:14:36 +0200

    Add files via upload

--- theJsWay/chapter09.js
+++ theJsWay/chapter09.js
@@ -1,10 +1,101 @@
-let day = prompt("Enter day.")
+// // let day = prompt("Enter day.")
 
-if (day === "Friday" &&  "Saturday" && "Sunday") {
-    console.log("Will code");
+// // if (day === "Friday" &&  "Saturday" && "Sunday") {
+// //     console.log("Will code");
+// // }
+
+// // else 
+// // {
+// //     console.log("Won't code");
+// // }
+
+
+// class Character{
+//     constructor(name, health, strength) {
+//         this.name = name;
+//         this.health = health;
+//         this.strength = strength;
+//         this.xp =0 ;
+
+//     }
+
+//     //return character description
+//     describe(){
+//         return `${this.name} has ${this.health} health points, ${this
+//             .strength} as strength and ${this.xp} XP points`;
+//         }
+// }
+
+
+// const aurora = new Character ("Aurora", 100, 120);
+// const max = new Character("Max", 70, 110);
+
+
+// //aurora is harmed by an arrow
+// aurora.health -= 20;
+
+// //aurora gains strength necklace
+// aurora.strength += 20;
+
+// //aurora gains experience.
+// aurora.xp+= 10;
+
+
+// console.log(aurora.describe());
+// console.log(max.describe());
+
+class Character {
+    constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.xp = 0; // XP is always zero for new characters
+    }
+
+
+attack(target) {
+    if (this.health > 0) {
+    const damage = this.strength;
+    console.log(
+    `${this.name} attacks ${target.name} and causes ${damage} damage points`
+    );
+target.health -= damage;
+if (target.health > 0) {
+console.log(`${target.name} has ${target.health} health points left`);
+} else {
+target.health = 0;
+const bonusXP = 10;
+console.log(
+`${this
+.name} eliminated ${target.name} and wins ${bonusXP} experience point\
+s`
+);
+this.xp += bonusXP;
+}
+} else {
+console.log(`${this.name} can't attack (they've been eliminated)`);
+}
+}
+// Return the character description
+describe() {
+return `${this.name} has ${this.health} health points, ${this
+.strength} as strength and ${this.xp} XP points`;
+}
 }
 
-else 
-{
-    console.log("Won't code");
-}
+
+const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
+console.log(aurora.describe());
+console.log(glacius.describe());
+
