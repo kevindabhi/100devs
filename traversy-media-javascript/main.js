078957df2ea234726aa5290c505db14d4cf78929
Kevin <kevindabhi@gmail.com>
2022-02-12 00:11:11 +0100

    started javascript

---
+++ traversy-media-javascript/main.js
@@ -0,0 +1,10 @@
+const string= "I am a software developer";
+let age = 32;
+
+
+console.log(string.split(""));
+
+var arrayname = [ "value1", "value2", "value3"]
+arrayname.unshift("red");
+
+console.log(arrayname);
