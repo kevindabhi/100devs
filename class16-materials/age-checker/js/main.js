509b0663c291dac2b8c906dcea7d178c02dc39c9
Kevin <kevindabhi@gmail.com>
2022-03-04 19:13:45 +0100

    practice

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -1,13 +1,46 @@
+h1Holder = document.querySelector('h1');
+h1Holder.addEventListener('click', logAge);
+
+function logAge(){
+
+let age = document.querySelector('#danceDanceRevolution').value
+console.log(age);
+
+if (age <16 ) {
+    console.log("You cannot drive")
+} else if (age < 18) {
+    console.log("Can't hate from outside the club because you can't even get in.")
+    }
+  else if (age <= 21)  {
+      console.log("you can not drink")
+  }
+
+  else if (age <= 25)  {
+    console.log("you can not rent cars affordably")
+}
+
+else if (age <= 30)  {
+    console.log("you can not rent fancy cars affordably")
+}
+
+else if (age > 30)  {
+    document.getElementById("paragraph").innerHTML= "Enjoy the ride";
+}
+
+}
 //Create a conditonal that checks their age
+
 //If under 16, tell them they can not drive
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
 //If under 21, tell them they can not drink
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
-//If under over 30, tell them there is nothing left to look forward too
+//If over 30, tell them there is nothing left to look forward too
 
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+
--- class16-materials/variable-fun-time/js/main.js
+++ class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,40 @@
 //--- Easy
 //create a variable and assign it a number
 
+let easy = 20;
+
 //minus 10 from that number
 
+easy = easy -10;
+
+
 //print that number to the console
+
+console.log(easy);
 
 //--- Medium
 //create a variable that holds a value from the input
 
-//add 25 to that number
 
-//alert that number
+
+
+// //add 25 to that number
+// medium = medium + 25;
+
+// //alert that number
+// alert(medium)
 
 //--- Hard
 //create a variable that holds the h1
 
+const h1Holder = document.querySelector('h1')
+
 //add an event listener to that element that console logs the sum of the two previous variables
+
+
+h1Holder.addEventListener('click', sum)
+
+function sum (){
+    let medium = document.querySelector('#danceDanceRevolution').value;
+    console.log(Number(easy) + Number(medium));
+}
