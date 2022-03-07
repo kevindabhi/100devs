eea6c60bf1c13811c86af276c00e8556f8da44b5
Kevin <kevindabhi@gmail.com>
2022-03-07 10:29:43 +0100

    eloquent js

---
+++ eloquentJs/main.js
@@ -0,0 +1,15 @@
+//Looping a triangle
+
+/*
+Pseudo code
+I want to print # , and then next lopp ## the number of # keep increasing until the loop gets to the counter.
+*/
+
+// function pound ()
+// {
+//     console.log("#");
+// }
+
+let abc = "#"
+
+console.log(abc)
