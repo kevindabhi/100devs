11f5b69dfce223610152f71a1dad0316ae822f37
Kevin <kevindabhi@gmail.com>
2022-03-18 23:14:55 +0100

    class 19

--- class19-materials-arrays/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials-arrays/bring-it-on-all-or-nothing/js/main.js
@@ -1,19 +1,38 @@
 // *Variables*
 // Declare a variable, assign it a boolean, and alert the value
 
+// let boolVariable = true;
+// alert(boolVariable)
+
 
 // Declare a variable, reassign it to your favorite color, and console log the value
-
+myFavoriteColor = "blue";
+myFavoriteColor  = "royal blue";
+console.log(myFavoriteColor);
 
 // *Functions*
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
+function fourNumbers(num1, num2, num3, num4) {
+    
+    return ( num1 + num2 + num3)/  num4;
+   
+}
 
+fourNumbers(1,2,4,7);
 
 // Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
+function power(num5, num6) {
+    console.log(Math.pow(num5,num6));
+}
+power(2,4);
 
 
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+
+function alertOrLog(boolean,string){
+    if boolean
+}
 
 
 //*Loops*
