2e77639a37c4f404719a5bb78d8a85f336eb3257
Kevin <kevindabhi@gmail.com>
2022-03-24 22:43:52 +0100

    js info

---
+++ jscode.info/objects.js
@@ -0,0 +1,69 @@
+// Write the code, one line for each action:
+
+// Create an empty object user.
+// Add the property name with the value Kevin.
+// Add the property surname with the value Dabhi.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+
+let user = {};
+user.name = "Kevin";
+user.surname = "Dabhi";
+user.name = "Pete";
+delete user.name;
+
+//second program
+
+let fuxToGive = {};
+
+alert( isEmpty(fuxToGive)); // true
+
+fuxToGive["just"] = "last one";
+
+alert( isEmpty(fuxToGive) ); // false
+
+
+//third
+
+
+function fuxToGive(obj) {
+    for (let key in obj) {
+      // if the loop has started, there is a property
+      return false;
+    }
+    return true;
+  }
+
+
+  //fourth
+
+  let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  
+  let sum = 0;
+  for (let key in salaries) {
+    sum += salaries[key];
+  }
+  
+  alert(sum); // 390
+
+
+  // before the call
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+  multiplyNumeric(menu);
+  
+  // after the call
+  menu = {
+    width: 400,
+    height: 600,
+    title: "My menu"
+  };
