59d4fb1ae476eb000a22b4371b65f263c3ebeb56
Kevin <kevindabhi@gmail.com>
2022-04-01 15:25:10 +0200

    code wars- reverse string.

--- codewars/main.js
+++ codewars/main.js
@@ -45,15 +45,42 @@
 
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
-function boolToWord( bool){
-   let x = bool;
-   if (bool === true) {
-       console.log("yes")
-   }
-   else{
-       console.log("No")
-   }
-   return
-  }
+// function boolToWord( bool){
+//    let x = bool;
+//    if (bool === true) {
+//        console.log("yes")
+//    }
+//    else{
+//        console.log("No")
+//    }
+//    return
+//   }
 
-  boolToWord(true)
+//   boolToWord(true)
+
+
+
+
+
+//Reversed Strings
+// Pseudo Code
+// First I want to have an array, then i want to put the array into another array starting from first word first till the last word in the end, since this is FILO i will then console log this array.
+
+// function solution(str){ 
+//     return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// solution("hello")
+
+// Code Explanation
+// Our goal is to take the input, str, and return it in reverse. Our first step is to split the string by characters using split(''). Notice that we don’t leave anything in between the single quotes, this tells the function to split the string by each character.
+
+// Using the split() function will turn our string into an array of characters, keep that in mind as we move forward.
+
+// Next we chain the reverse() function, which takes our array of characters and reverses them.
+
+// Finally, we chain join('') to put our characters back together into a string. Notice once again that we left no spaces in the argument for join, this makes sure that the array of characters is joined back together by each character.
+
