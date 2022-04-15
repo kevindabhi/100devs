3d2bd5098735a664887949594c853b0570c1843a
Kevin <kevindabhi@gmail.com>
2022-04-15 16:51:28 +0200

    jsinfo

---
+++ javascript-info/objects/script.js
@@ -0,0 +1,22 @@
+// let user = {
+//     name : "Kevin",
+//     age : 32,
+// };
+
+
+
+// console.log(user.name);
+// console.log(user.age);
+
+
+// let key = prompt("What do you want to know about the user?", "name");
+
+// // access by variable
+// alert( user[key] ); // John (if enter "name")
+
+
+let fruit = prompt("Which fruit to buy?", "apple");
+let bag = {};
+
+// take property name from the fruit variable
+bag[fruit] = 5;
