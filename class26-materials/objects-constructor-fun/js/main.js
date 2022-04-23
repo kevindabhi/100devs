fcbd6c3b288fa7381f812550c79091328b20d6c7
Kevin <kevindabhi@gmail.com>
2022-04-24 00:29:56 +0200

    updates

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+
+function PizzaMachine(size, toppings, crust, sauce) {
+    this.size = size
+    this.toppings= toppings
+    this.crust = crust
+    this.sauce= sauce
+    this.estimatedTime = function(){
+        console.log ("calculating")
+    }
+    this.burnMouth = function(){
+        console.log ("asfasgasdg")
+    }
+    this.frisbee= function(){
+        console.log ("yeet")
+    }
+}
+
+
+let veganPizza = new PizzaMachine( "30", ["vegan sauce", "onions", "paprika"], "cheese burst", "special" )
+
+PizzaMachine.prototype.cheese = false;
--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,18 @@
 //Create a dog object that has four properties and three methods
+let pizza = {}
+pizza.size = 30
+pizza.brand = "Dominos"
+pizza.type = "vegan"
+pizza.isTasty = true
+pizza.toppings = ["spinach", "onion", "vegancurry"]
+pizza.estimateTime = function() {
+    console.log("calculating...")
+}
+
+pizza.burnMouth = function() {
+    console.log("asfafkfgdfg.")
+}
+pizza.frisbee= function() {
+    console.log("yeeet")
+}
+
