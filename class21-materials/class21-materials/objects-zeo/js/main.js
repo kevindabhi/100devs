4ca64e80a9ddd09669dbf9bc019b6fb3e1ac647c
Kevin <kevindabhi@gmail.com>
2022-04-23 16:46:04 +0200

    practice and added class 22 and 23 files.

--- class21-materials/class21-materials/objects-zeo/js/main.js
+++ class21-materials/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,15 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+
+stopwatch.color = "black"
+stopwatch.shape = "square"
+stopwatch.company = "nokia"
+stopwatch.owner="Kevin"
+
+stopwatch.tellBrandName = function(company)
+{
+    console.log(`the brandname is ${company}`)
+}
+ 
+stopwatch.tellBrandName(stopwatch.company);
