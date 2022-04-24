4885c11e593d623792f216cf7cc21fe4d459376d
Kevin <kevindabhi@gmail.com>
2022-04-24 19:03:51 +0200

    updates

--- class28-materials/api-template/js/main.js
+++ class28-materials/api-template/js/main.js
@@ -4,14 +4,14 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+  const url = `https://www.dnd5eapi.co/api/spells/?name=Acid+Arrow`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
         if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
+          document.querySelector('img').src = data.hdurl 
         }else if(data.media_type === 'video'){
           document.querySelector('iframe').src = data.url
         }else{
