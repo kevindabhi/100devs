1278dfbfd78669947ec3c2a71869c81d461eea67
Kevin <kevindabhi@gmail.com>
2022-04-24 15:15:05 +0200

    class 28 files

--- class27-materials/card-game/js/main.js
+++ class27-materials/card-game/js/main.js
@@ -1,12 +1,28 @@
 //Example fetch using pokemonapi.co
-let deckID=""
-document.querySelector('button').addEventListener('click', drawTwo)
+document.querySelector('button').addEventListener('click', getFetch)
 
-function drawTwo(){
+function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
   const url = `https://api.nasa.gov/planetary/apod?api_key=JqgDsgC9C1Is1wLLuBmysYMV5MJbDFxTmbgX6hu5&date=${choice}`
 
- 
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if (data.media_type === "image" ){
+          document.querySelector("img").src= data.hdurl
+        }
+       
+          else if (data.media_type === "video") {
+            document.querySelector("iframe").src= data.url
+
+          }
+        
+        document.querySelector("h3").innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }
 
