668d9db9ac5669d79be8bf055894da8602b685d2
Kevin <kevindabhi@gmail.com>
2022-05-04 22:06:26 +0200

    updates

--- class26-materials/api-template/js/main.js
+++ class26-materials/api-template/js/main.js
@@ -9,7 +9,7 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-      })
+      })  
       .catch(err => {
           console.log(`error ${err}`)//
       });
