511a897ced97280cd9521ba3ebe7802cb3dcdac6
Kevin <kevindabhi@gmail.com>
2022-05-18 08:21:49 +0200

    updates

--- class14-materials-loops/class14-materials/bachelor-code/2/js/main.js
+++ class14-materials-loops/class14-materials/bachelor-code/2/js/main.js
@@ -18,6 +18,7 @@ function claireNext(){
 	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
 }
 
+
 function sharleenNext(){
 	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
 	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
