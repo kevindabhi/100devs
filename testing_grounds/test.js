4f199e005fd7c4ea6fe86c0d006b9caafc705b9e
Kevin <kevindabhi@gmail.com>
2022-07-17 18:17:57 +0200

    update

---
+++ testing_grounds/test.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return `${title} by ${author}, it has ${pages} pages and I have ${read} it`
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "JRR", "99", "absolutely")
+
+console.log(theHobbit.info())
