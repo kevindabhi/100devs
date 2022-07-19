05dc9756f0c839d1e26a00aa785960c3a3cb3948
Kevin <kevindabhi@gmail.com>
2022-07-19 18:23:42 +0200

    updates

---
+++ book_library/js/main.js
@@ -0,0 +1,13 @@
+
+		let v = "The respective values are :"; 
+		function ns() { 
+			let input = document.getElementsByName('array[]'); 
+
+			for (let i = 0; i < input.length; i++) { 
+				let p = input[i]; 
+				v = v + "array[" + i + "].value= "	+ p.value + " "; 
+			} 
+
+			document.getElementById("let").innerHTML = v; 
+			document.getElementById("mv").innerHTML = "Output"; 
+		} 
