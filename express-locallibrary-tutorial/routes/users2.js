228f13c2db873f936710cf4221cde978ffab343f
Kevin <kevindabhi@gmail.com>
2022-08-24 11:14:32 +0200

    mdn

--- express-locallibrary-tutorial/routes/users2.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('users2', { title: 'user' });
-});
-
-module.exports = router;
