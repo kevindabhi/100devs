7eb319551d0578251d504dcc1d8455e6bb0c7917
Kevin <kevindabhi@gmail.com>
2022-08-24 09:21:37 +0200

    MDN library tutorial

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -41,3 +41,12 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
