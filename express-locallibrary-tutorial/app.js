176d1241950bc3d21bd817aba442d663e03d8cee
Kevin <kevindabhi@gmail.com>
2022-08-24 19:57:30 +0200

    MDN library working with views

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +41,15 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
+//Database
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017/local-library';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
