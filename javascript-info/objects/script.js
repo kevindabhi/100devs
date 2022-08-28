7578e58a79654ff2c177c51b7cc1f03b8346c518
Kevin <kevindabhi@gmail.com>
2022-08-28 15:35:36 +0200

    updates

--- javascript-info/objects/script.js
+++ javascript-info/objects/script.js
@@ -1,29 +1,52 @@
-// let user = {
-//     name : "Kevin",
-//     age : 32,
-// };
+// let age = prompt ("oi how old are you m8?")
+
+// let message = (age <10 ) ? "your're a child":  
+//                 (age < 18) ? "you're a big child":
+//                 (age<100) ? "you are an adult!":
+//                 "Oi you havin a giggle m8?"
 
 
 
-// console.log(user.name); 
-// console.log(user.age);
+// alert(message)                
+                
+
+// Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+
+// If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
 
 
-// let key = prompt("What do you want to know about the user?", "name");
+// let answer = prompt("what is the official name of javascript?")
 
-// // access by variable
-// alert( user[key] ); // John (if enter "name")
-//! 
+// if (answer == "ECMAScript") {
+//     alert("correct");
+// } 
+// else {
+//     alert("incorrect")
+// }
 
-// let fruit = prompt("Which fruit to buy?", "apple");
-// let bag = {};
+// let a = 5;
+// let b = 10
+// let result = a + b;
+//  answer = (result< 4) ? 'Below': 'over'
 
-// // take property name from the fruit variable
-// bag[fruit] = 5;
+// alert(answer)
 
-let user = {
-    name : "Kevin Dabhi",
-    age : 32,
-    isAdmin : true,
-};
+
+// let message;
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+
+
+// let message = (login ==" employee") ? "hello":
+//             (login ==" director") ? "Greetings":
+//             (login == "")? "no login":
+//             ""
 
