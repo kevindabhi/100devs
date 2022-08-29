ac505bd4657590d19229fb96a44e2fe18b2dd515
Kevin <kevindabhi@gmail.com>
2022-08-29 17:13:45 +0200

    The Odin Project Library

--- the-odin-project/library/index.js
+++ the-odin-project/library/index.js
@@ -1,11 +1,10 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        console.log(`Book ${this.title} written by ${this.author} has ${this.pages} pages and I have ${this.read} read it`)
+let myLibrary= [];
+class Book {
+    constructor(title, author, page, status){
+    this.title=title;
+    this.author=author;
+    this.page=page;
+    this.status=status;
     }
 }
 
-const hobbit = new Book("hobbit", "JRR", 150, "not")
