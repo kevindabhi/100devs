4a924cabda3b715d4cbe0b6bb4398f000b2eceb4
Kevin <kevindabhi@gmail.com>
2022-09-05 13:04:38 +0200

    updates

---
+++ codewars/Array/javascript_info_practice.js
@@ -0,0 +1,41 @@
+// let fruits = ["Apple", "Orange", "Pear"];
+// console.log(fruits +" " + "before popping")
+// fruits.pop()
+
+const { startSession } = require("mongoose")
+
+// console.log(fruits)
+
+// fruits.push("Mango")
+// console.log(fruits)
+
+// fruits.unshift("Guava")
+// console.log(fruits)
+
+str = "this is a string"
+
+// let count = 1
+// for (var i = 0; i < str.length; i++) {
+//     if(str[i] == "a") {
+//         count++
+//     }
+//     else if (str[i] == "e"){
+//         count++
+//     }
+//     else if (str[i] == "i"){
+//         count++
+//     }
+//     else if (str[i] == "o"){
+//         count++
+//     }
+//     else if (str[i] == "u"){
+//         count++
+//     }
+//   }
+
+//   console.log(count)
+
+
+strnew = str.replace("a", "")
+console.log(strnew);
+
