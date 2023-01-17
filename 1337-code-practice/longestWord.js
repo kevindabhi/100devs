746e926cf304de999db4235d96a0f87179aa0752
Kevin Dabhi <kevindabhi@gmail.com>
2023-01-17 16:55:48 +0100

    test

--- 1337-code-practice/longestWord.js
+++ 1337-code-practice/longestWord.js
@@ -1,16 +1,13 @@
-function longestWord(str)
-{
-    let maxLength =0
-    let words = str.split(' ')
-    for(let i = 0; i<words.length; i++)
-    {
-        if(words[i].length>maxLength)
-        {
-            maxLength = words[i].length
-        }
+function longestWord(str) {
+  let maxLength = 0;
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
     }
-    console.log(maxLength)
+  }
+  console.log(maxLength);
 }
-longestWord ("Hello algoritm and leetcode is quite difficult at the moment.")
+longestWord("Hello algoritm and leetcode is quite difficult at the moment.");
 
-//nothing done today but just doing git to keep consistency 
+//nothing done today but just doing git to keep consistency
