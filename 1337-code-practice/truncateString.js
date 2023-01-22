3e1f281ecd66581fc20fd64f89b946c2f976f38c
Kevin Dabhi <kevindabhi@gmail.com>
2023-01-22 12:19:47 +0100

    leetcode

---
+++ 1337-code-practice/truncateString.js
@@ -0,0 +1,9 @@
+function truncateString(str, num) {
+  if (str.length > num) {
+    console.log(str.slice(0, num) + "...");
+  } else {
+    console.log(str);
+  }
+}
+
+truncateString("hey how are you?", 5);
