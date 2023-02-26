eb65609cff1006c93baa6069a5aa075d47166aa6
Kevin Dabhi <36810148+kevindabhi@users.noreply.github.com>
2023-02-26 10:40:42 +0100

    tic tac toe game from react beta docs.

--- ttt/src/app.js
+++ ttt/src/app.js
@@ -7,7 +7,6 @@ function Square({value, onSquareClick}) {
     </button>
   );
 }
-
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
